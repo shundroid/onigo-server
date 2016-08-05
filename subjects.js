503d57539d1e1e46c714a3a00ae4054a5ebd24bd
shundroid <shundroid@users.noreply.github.com>
2016-08-05 15:53:05 +0900

    subjectを増やした

--- subjects.js
+++ subjects.js
@@ -1,7 +1,13 @@
-import { BehaviorSubject } from "rx";
+import { BehaviorSubject, Subject } from "rx";
 
 export default {
   gameState: new BehaviorSubject("inactive"),
   rankingState: new BehaviorSubject("hide"),
-  availableCommandsCount: new BehaviorSubject(1)
+  availableCommandsCount: new BehaviorSubject(1),
+  orbs: new BehaviourSubject({}),
+  currentLog: new BehaviourSubject(""),
+  controllers: new BehaviourSubject({}),
+  unnamedClients: new BehaviourSubject([]),
+  // ここにデータのない通知（ping、batteryなど）が流れてくる
+  notifications: new Subject()
 };
