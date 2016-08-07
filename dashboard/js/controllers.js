3b709cac2ce85ec4d6c36e7c3142959dcff2e12a
shundroid <shundroid@users.noreply.github.com>
2016-08-07 13:08:28 +0900

    knockoutのviewmodelをclassに

--- dashboard/js/controllers.js
+++ dashboard/js/controllers.js
@@ -1,10 +1,12 @@
 import ko from "knockout";
 import eventPublisher from "./publisher";
 
-export default function Controllers() {
-  this.controllers = ko.observableArray([]);
-  eventPublisher.on("controllers", controllers => {
-    this.controllers.removeAll();
-    this.controllers.push.apply(this.controllers, controllers);
-  });
+export default class Controllers {
+  constructor() {
+    this.controllers = ko.observableArray([]);
+    eventPublisher.on("controllers", controllers => {
+      this.controllers.removeAll();
+      this.controllers.push.apply(this.controllers, controllers);
+    });
+  }
 }
--- dashboard/js/unnamedClients.js
+++ dashboard/js/unnamedClients.js
@@ -1,10 +1,12 @@
 import ko from "knockout";
 import eventPublisher from "./publisher";
 
-export default function UnnamedClients() {
-  this.unnamedClients = ko.observableArray([]);
-  eventPublisher.on("unnamedClients", unnamedClients => {
-    this.unnamedClients.removeAll();
-    this.unnamedClients.push.apply(this.unnamedClients, unnamedClients);
-  });
+export default class UnnamedClients {
+  constructor() {
+    this.unnamedClients = ko.observableArray([]);
+    eventPublisher.on("unnamedClients", unnamedClients => {
+      this.unnamedClients.removeAll();
+      this.unnamedClients.push.apply(this.unnamedClients, unnamedClients);
+    });
+  }
 }
